9926324032bc73ef8e355fc54daa0a5c23c7e1e1
ChristianLofqvist <christian.lofqvist@iths.se>
2022-02-23 13:55:05 +0100

    Add StudentService class

--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -1,7 +1,42 @@
 package se.iths.service;
 
+import se.iths.entity.Student;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+
+@Transactional
 public class StudentService {
 
+    @PersistenceContext
+    EntityManager entityManager;
 
+    public void createStudent(Student student) {
+        entityManager.persist(student);
+    }
+
+    public void updateStudent(Student student, Long id) {
+        if (!Objects.equals(student.getId(), id))
+            throw new IllegalStateException("Provided student IDs do not match");
+        entityManager.merge(student);
+    }
+
+    public List<Student> getAllStudentsByLastName(String lastName) {
+        TypedQuery<Student> query = entityManager.createQuery("SELECT s FROM Student s WHERE s.lastName LIKE :lastName", Student.class);
+        query.setParameter("lastName", "%" + lastName + "%");
+        return query.getResultList();
+    }
+
+    public void deleteStudent(Long id) {
+        Student foundStudent = entityManager.find(Student.class, id);
+        entityManager.remove(foundStudent);
+    }
+
+    public List<Student> getAllStudents() {
+        return entityManager.createQuery("SELECT s FROM Student s", Student.class).getResultList();
+    }
 }

7f8930351d9e57a3d40e030336802885199e4a95
ChristianLofqvist <christian.lofqvist@iths.se>
2022-02-24 09:49:08 +0100

    Add getters and setters for ExceptionsAsJson

--- src/main/java/se/iths/exceptions/ExceptionsAsJson.java
+++ src/main/java/se/iths/exceptions/ExceptionsAsJson.java
@@ -13,4 +13,28 @@ public class ExceptionsAsJson {
         this.errorMessage = errorMessage;
         this.errorCode = errorCode;
     }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
 }
